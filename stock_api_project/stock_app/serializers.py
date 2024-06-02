a216bd87d63eb5511d9f16426d0e737c55392841
diegomunozme <diego.munoz.me@gmail.com>
2024-06-02 15:53:18 -0600

    cleaned old project from repo

--- stock_api_project/stock_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Stock
-
-class StockDailySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Stock
-        fields = ('date', 'open_price', 'high_price', 'low_price', 'close_price', 'volume')
--- stock_api_project/stock_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import StockDailyView
-
-urlpatterns = [
-    path('stock-daily/', StockDailyView.as_view(), name='stockdailyview'),
-]
